dc24a58613f4313ece78370a73ad7c34bfee5361
Felipe García <felipe@toptierlabs.com>
2016-09-13 14:14:24 -0300

    Added Comment listing and submitting form

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -3,8 +3,9 @@ class CommentsController < ApplicationController
 
   def create
     @comment = current_user.comments.build(comment_params)
-
+    @comment.book_id = params[:book_id]
     @comment.save
+
     redirect_to :back
   end
 
